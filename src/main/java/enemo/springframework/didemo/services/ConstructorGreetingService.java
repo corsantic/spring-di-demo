6ae0c9ba13d2bca5d881c8a57bc1759456872765
corsantic <aydofurkan25@gmail.com>
2018-11-20 21:12:40 +0300

    qualifiers

---
+++ src/main/java/enemo/springframework/didemo/services/ConstructorGreetingService.java
@@ -0,0 +1,14 @@
+package enemo.springframework.didemo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConstructorGreetingService implements GreetingService {
+
+
+    @Override
+    public String sayGreeting(){
+        return "Hello -  I was Injected via the Constructor";
+    }
+
+}
---
+++ src/main/java/enemo/springframework/didemo/services/SetterGreetingService.java
@@ -0,0 +1,13 @@
+package enemo.springframework.didemo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetterGreetingService implements GreetingService {
+
+
+    @Override
+    public String sayGreeting(){
+        return "Hello -  I was Injected by Getter";
+    }
+}
