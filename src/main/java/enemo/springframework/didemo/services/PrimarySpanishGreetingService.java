9d806d079a208f69fd84fecebaf737d7f060d0fb
corsantic <aydofurkan25@gmail.com>
2018-11-21 15:43:24 +0300

    configuration

--- src/main/java/enemo/springframework/didemo/services/PrimarySpanishGreetingService.java
+++ src/main/java/enemo/springframework/didemo/services/PrimarySpanishGreetingService.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service
-@Profile("es")
-@Primary
 public class PrimarySpanishGreetingService  implements GreetingService{
 
 
